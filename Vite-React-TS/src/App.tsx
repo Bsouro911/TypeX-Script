c313c593e5be15ae36fddd30e1bd5309d4707e19
Bsouro911 <hopepope911@gmail.com>
2023-11-06 23:18:50 +0530

    React fundamentals with Ts

--- Vite-React-TS/src/App.tsx
+++ Vite-React-TS/src/App.tsx
@@ -1,11 +1,48 @@
-import Heading from "./components/Heading";
-import Section from "./components/Section";
+// import Counter from "./components/Counter";
+// import Heading from "./components/Heading";
+// import Section from "./components/Section";
+// import { useState, useEffect, useCallback, useMemo } from "react";
+// import List from "./components/List";
+
+// interface User {
+//   id: number;
+//   name: string;
+// }
+import Counter from "./components/Counter";
+import { CounterProvider } from "./context/CounterContext";
 
 function App() {
+  // const [count, setCount] = useState<number>(0);
+  // const [users, setUsers] = useState<User[] | null>(["souro"]);
+  // useEffect(() => {
+  //   console.log("mounting");
+  //   // console.log("Users", users);
+
+  //   return () => {
+  //     console.log("unmounting");
+  //   };
+  // }, [users]);
+
+  // const add = useCallback((): void => setCount((prev) => prev + 1), []);
+
   return (
     <>
-      <Heading title={"souro"} />
+      {/* <Heading title={"souro"} />
       <Section title={"This is my title"}>This is my section</Section>
+      <Counter setCount={setCount}>Count is: {count} </Counter>
+      <List
+        items={["☕ Coffee", "🌮 Tacos", "💻 Code"]}
+        render={(item: string) => <span className="bold">{item}</span>}
+      /> */}
+      {/* <div>
+        <p>{count}</p>
+        <button onClick={add}>+</button>
+      </div> */}
+      {/* <p>{users}</p> */}
+      {/* <Counter>{(num: number) => <>Current Count: {num}</>}</Counter> */}
+      <CounterProvider>
+        <Counter>{(num: number) => <>Current Count: {num}</>}</Counter>
+      </CounterProvider>
     </>
   );
 }
